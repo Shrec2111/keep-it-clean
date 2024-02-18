d582dd45f61bda1083b47cea95be8e862e80bcd9
Shrec2111 <77185542+Shrec2111@users.noreply.github.com>
2024-02-18 19:14:54 +0530

    Create app.js for testing

---
+++ tester/app.js
@@ -0,0 +1,41 @@
+//test scenarios
+//you can replace the code from testFile1/2.js to check the results.
+
+const fs = require('fs');
+const codeQualityChecker = require('code-preetier');
+
+// File to test
+const filePath = './file1.js'; // this is a dummy file which contains unformatted data.
+
+// Read code from file
+const code = fs.readFileSync(filePath, 'utf8');
+
+// 1. Linting Test
+(async () => {
+  try {
+    const lintResults = await codeQualityChecker.lint([filePath]);
+    console.log('Linting Results:', lintResults);
+  } catch (error) {
+    console.error('Error occurred during linting:', error);
+  }
+})();
+
+// 2. Static Analysis Test
+(async () => {
+  try {
+    const staticAnalysisResults = await codeQualityChecker.staticAnalysis([filePath]);
+    console.log('Static Analysis Results:', staticAnalysisResults);
+  } catch (error) {
+    console.error('Error occurred during static analysis:', error);
+  }
+})();
+
+// 3. Formatting Test
+(async () => {
+  try {
+    const formattedCode = await codeQualityChecker.format(code);
+    console.log('Code formatted successfully:', formattedCode);
+  } catch (error) {
+    console.error('Error occurred during formatting:', error);
+  }
+})();
